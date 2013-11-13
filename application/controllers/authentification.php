01aa28a87db20f26d4a83a35262d0044a2d9986f
Bastian <bastian.peghaire@gmail.com>
2013-11-13 16:26:07 +0100

    [Git] Test install on new computer

--- application/controllers/authentification.php
+++ application/controllers/authentification.php
@@ -1,54 +1,49 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
 
-class Authentification extends CI_Controller
-{
+if (!defined('BASEPATH'))
+  exit('No direct script access allowed');
 
-/*
-======================================
-			CONSTRUCTEUR
-======================================
-*/
+class Authentification extends CI_Controller {
+  /*
+    ======================================
+    CONSTRUCTEUR
+    ======================================
+   */
 
-	public function __construct()
-	{
-		parent::__construct();
-		
-		//	Paramétrages du Layout
-		$this->layout->set_theme('blank');
-		$this->layout->set_titre('SLA | Authentification');
-		$this->form_validation->set_error_delimiters('<div class="alert alert-warning"><a class="close" data-dismiss="alert"></a>', '</div>');
+  public function __construct() {
+    parent::__construct();
 
-	}
+    //	Paramétrages du Layout
+    $this->layout->set_theme('blank');
+    $this->layout->set_titre('SLA | Authentification');
+    $this->form_validation->set_error_delimiters('<div class="alert alert-warning"><a class="close" data-dismiss="alert"></a>', '</div>');
+  }
 
-/*
-======================================
-			FONCTIONS
-======================================
-*/
+  /*
+    ======================================
+    FONCTIONS
+    ======================================
+   */
 
-	public function index()
-	{		
-		$this->connect();
-	}
+  public function index() {
+    $this->connect();
+  }
 
-	public function connect()
-	{
-		//	Form Validation
-		if ( $this->form_validation->run('connect') )
-		{
-			//	Validation des infos
-			$this->authentification->connect( $this->input->post() );
-		}
-		
-		$this->layout->view('utilities/connect');
-	}
-	
-	public function disconnect()
-	{
-		$this->authentification->disconnect();
-	}
-	
-	
+  public function connect() {
+    //	Form Validation
+    if ($this->form_validation->run('connect')) {
+      //	Validation des infos
+      $this->authentification->connect($this->input->post());
+    }
+
+    $this->layout->view('utilities/connect');
+  }
+
+  public function disconnect() {
+    $this->authentification->disconnect();
+  }
+
 }
+
 /* End of file authentification.php */
 /* Location: ./application/controllers/authentification.php */
