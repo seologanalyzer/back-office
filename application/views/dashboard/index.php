32413c3fa3277f94a99e8201ea6962062346de73
Pierre Grimaud <grimaud.pierre@gmail.com>
2013-11-14 19:30:51 +0100

    Analyse function

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -1,1 +1,26 @@
 <h3 class="page-title">Tableau de bord</h3>
+<div class="row-fluid">
+  <div class="span12">
+    <div class="portlet box green">
+      <div class="portlet-title">
+        <div class="caption">
+          <i class="icon-cogs"></i>
+          Analyse des logs
+        </div>
+        <div class="tools">
+          <a class="collapse" href="javascript:;"></a>
+        </div>
+      </div>
+      <div class="portlet-body form">
+        <form class="form-horizontal no-margin">
+          <div class="control-group no-margin">
+            <div class="span10">Votre dernière analyse date du DATE. Vous pouvez en lancer une nouvelle en cliquant sur le bouton ci-contre.</div>
+            <div class="span2">
+              <a href=""><input type="button" value="Lancer" class="btn green" /></a>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</div
